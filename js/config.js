e744dc10a967257b3f01b0145b48c7502d95ed44
davletmegahorosh <117630956+davletmegahorosh@users.noreply.github.com>
2024-11-19 10:16:58 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_dGX2TJS7w', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '145o2ek8i0tbjah70rudrhepa8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
